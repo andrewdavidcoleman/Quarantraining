e7a7caf1fc1469f47bb81f5c0297badc41c5de5f
Andrew Coleman <andrewdavidcoleman@gmail.com>
2020-04-06 13:17:31 -0400

    Service logic to take handle bad data and programmatically move "DU" metcons to the pregame for that day.

--- Data/InMemoryDbService.cs
+++ Data/InMemoryDbService.cs
@@ -25,15 +25,32 @@ namespace Quarantraining.Data
                     var pregames = pregameCsv.GetRecords<dynamic>().ToList();
                     var metcons = metconCsv.GetRecords<dynamic>().ToList();
 
+                    // If a metcon is listed as max DU's, move it to the pregame for that date, and remove from metcons
+                    var doubleUnderMetcons = metcons.Where(m => m.Name == "Max Double-Unders" || m.Name == "2min Double-Unders");
+                    foreach (var doubleUnderMetcon in doubleUnderMetcons)
+                    {
+                        // Get the first pregame that has the same date as this doubleUnderMetcon
+                        // Add "2 min DU's..." to the beginning of the pregame
+                        var pregameToAddDUTo = pregames.FirstOrDefault(p => p.Date == doubleUnderMetcon.Date);
+                        if (pregameToAddDUTo != null)
+                        {
+                            pregames.FirstOrDefault(p => p.Date == doubleUnderMetcon.Date).Component = pregameToAddDUTo.Component.Insert(0, "2min Double-Unders:\nGet as many double-unders as possible in two minutes, record your largest unbroken set.\n\n");
+                        }
+                    }
+
+                    // Remove bad data of entire metcons of 2 min of DU's
+                    metcons.RemoveAll(m => m.Name == "2min Double-Unders" || m.Name == "Max Double-Unders");
 
                     for (int i = 1; i < metcons.Count; i++)
                     {
-                        var pregame = pregames.FirstOrDefault(p => p.Date == metcons[i].Date) ?? new { Id = i, Component = "" };
+                        // Get a list of all the pregame components for the day of this metcon
+                        var pregameItems = pregames.Where(p => p.Date == metcons[i].Date).Select(p => p.Component).ToList();
 
                         context.Pregames.Add(new Pregame()
                         {
                             Id = i,
-                            Component = pregame.Component
+                            // If pregame records have the same date, aggregate their components together separated by line breaks
+                            Component = pregameItems.Count > 0 ? pregameItems.Aggregate((current, next) => current + "\n" + next).ToString() : ""
                         });
 
                         context.Metcons.Add(new Metcon()
