74ec907a80a4cacf0d3a928f8df1476558d0b592
Andrew Coleman <andrewdavidcoleman@gmail.com>
2020-04-25 22:00:32 -0400

    Changed mysql version

--- Startup.cs
+++ Startup.cs
@@ -34,7 +34,7 @@ namespace Quarantraining
                 options => options
                 .UseMySql(Configuration.GetConnectionString("QuarantrainingDb"),
                 mySqlOptions => mySqlOptions
-                .ServerVersion(new Version(8, 0, 19), ServerType.MySql)
+                .ServerVersion(new Version(5, 6), ServerType.MySql)
                 .EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null)), ServiceLifetime.Transient);
 
             services.AddTransient<DataAccess>();
