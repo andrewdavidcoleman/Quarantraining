efb76c59a1b201e8696d0325e7eaa9d5fa018194
Andrew Coleman <andrewdavidcoleman@gmail.com>
2020-04-25 22:21:49 -0400

    Put release connection string in development appsettings.

--- Startup.cs
+++ Startup.cs
@@ -35,7 +35,7 @@ namespace Quarantraining
                 .UseMySql(Configuration.GetConnectionString("QuarantrainingDb"),
                 mySqlOptions => mySqlOptions
                 .ServerVersion(new Version(5, 5, 62), ServerType.MySql)
-                .EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null)), ServiceLifetime.Transient);
+                .EnableRetryOnFailure(3, TimeSpan.FromSeconds(10), null)), ServiceLifetime.Transient);
 
             services.AddTransient<DataAccess>();
         }
